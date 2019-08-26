4f476651428bbb2776e6855bbcab8a130c454094
Stephen Parker <stephenparkhum@outlook.com>
2019-08-26 16:20:27 -0700

    Updating Add Tour form

--- src/components/AddTour/AddTour.js
+++ src/components/AddTour/AddTour.js
@@ -7,17 +7,17 @@ class AddTour extends Component {
             <div>
             <h2>Add Tour</h2>
             <form className="add-show-form">
-                <label for="date">Date</label>
-                <input type="date" name="date" id="date" />
-                <label for="location">Location</label>
-                <input type="text" name="location" id="location" />
-                <label for="venue">Venue</label>
-                <input type="text" name="venue" id="venue" />
+                <label for="start-date">Start Date</label>
+                <input type="date" name="start-date" id="start-date" />
+                <label for="end-date">End Date</label>
+                <input type="date" name="end-date" id="end-date" />
+                <label for="country">Country</label>
+                <input type="text" name="country" id="country" />
                 <label for="support-band">Support Bands</label>
                 <input type="text" name="support-band" id="support-band" />
                 <label for="fb-event-link">Facebook Event Link</label>
                 <input type="text" name="fb-event-link" id="fb-event-link" />
-                <input type="submit" value="Add Show" />
+                <input type="submit" value="Add Tour" />
                 <button className="cancel-btn">Cancel</button>
             </form>
             </div>
