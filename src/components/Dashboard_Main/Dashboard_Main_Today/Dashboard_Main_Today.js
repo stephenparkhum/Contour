53c43e83d0103fc72542d023a85b52f318cbd6d6
Stephen Parker <stephenparkhum@outlook.com>
2019-08-27 11:28:46 -0700

    Updating TODAY widget

--- src/components/Dashboard_Main/Dashboard_Main_Today/Dashboard_Main_Today.js
+++ src/components/Dashboard_Main/Dashboard_Main_Today/Dashboard_Main_Today.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Dashboard_Main_Today.css';
 
 function DashboardMainToday() {
     return (
         <>
         <h2>Today</h2>
+        <div className="dashboard-today-widget">
+            <h3>Portland, OR</h3>
+            <p><small>8/27/2109</small></p>
+            <ul>
+                <li><strong>Venue:</strong> High Water Mark</li>
+                <li><strong>Address:</strong> 1234 Main St, Portland, Oregon</li>
+                <li><strong>Load-In</strong> 2pm</li>
+                <li><strong>Soundcheck:</strong> 5pm</li>
+                <li><strong>Set Time:</strong> 10pm</li>
+            </ul>
+        </div>
         </>
     )
 }
